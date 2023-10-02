0d48ac7b6cfe477607ace77d55c8de8e9a1954d1
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-10-01 20:51:59 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int age;
+
+        System.out.println("Please enter your age.");
+        if(scan.hasNextInt());{
+            age = scan.nextInt();
+        }
+        if (age >= 21){
+            System.out.println("You get a wristband.");
+        }
     }
 }
